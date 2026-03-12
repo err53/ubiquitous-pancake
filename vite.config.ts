d08ae9ce6d734059ad775c216eca9ed37b58b3ac
Jason Huang <jasonhuang20035@gmail.com>
2026-03-12 13:13:48 -0400

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,10 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    host: "0.0.0.0",
+    allowedHosts: [
+      "yukari.eagle-rigel.ts.net",
+    ]
+  }
 });
